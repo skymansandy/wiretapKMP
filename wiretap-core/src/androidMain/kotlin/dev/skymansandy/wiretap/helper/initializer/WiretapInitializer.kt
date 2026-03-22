261f90e088213be479c4ca5ba6543cf06a0deabb
P Sandesh Baliga <iamsandythedev@gmail.com>
2026-03-22 13:11:15 +0530

    fix: resource reading context (#18)
    * Fix resource reading context
    
    * Fix detekt

--- wiretap-core/src/androidMain/kotlin/dev/skymansandy/wiretap/helper/initializer/WiretapInitializer.kt
+++ wiretap-core/src/androidMain/kotlin/dev/skymansandy/wiretap/helper/initializer/WiretapInitializer.kt
@@ -3,15 +3,21 @@ package dev.skymansandy.wiretap.helper.initializer
 import android.content.Context
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.startup.Initializer
 import dev.skymansandy.wiretap.helper.launcher.WiretapNotificationManager
 import dev.skymansandy.wiretap.helper.launcher.getLaunchIntent
+import dev.skymansandy.wiretap.helper.launcher.wiretapIconResId
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.setResourceReaderAndroidContext
 
 internal class WiretapInitializer : Initializer<Unit> {
 
+    @OptIn(ExperimentalResourceApi::class)
     override fun create(context: Context) {
         WiretapContextProvider.init(context)
+        setResourceReaderAndroidContext(context)
         WiretapNotificationManager.createChannel(context)
         addWiretapShortcut(context)
     }
@@ -22,6 +28,7 @@ internal class WiretapInitializer : Initializer<Unit> {
             val shortcut = ShortcutInfo.Builder(context, "wiretap_inspector")
                 .setShortLabel("Wiretap")
                 .setLongLabel("Open Wiretap Console")
+                .setIcon(Icon.createWithResource(context, wiretapIconResId(context)))
                 .setIntent(getLaunchIntent())
                 .build()
             shortcutManager.dynamicShortcuts = listOf(shortcut)
